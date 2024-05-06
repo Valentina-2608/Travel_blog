d3a17fa34e16ba27a00e575e762e73b046229b1e
Valetina-2608 <valusha2608@gmail.com>
2024-05-06 20:18:16 +0300

    Add script for log in

---
+++ src/script2.js
@@ -0,0 +1,72 @@
+/* Log In Page */
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+
+  import { getAuth,  signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"
+	
+  import { getDatabase, ref, update} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDQ2tgXiFtl1j-JyL0vz3ihq4YqpjQ6-Ls",
+    authDomain: "travel-blog-77d0f.firebaseapp.com",
+    projectId: "travel-blog-77d0f",
+    storageBucket: "travel-blog-77d0f.appspot.com",
+    messagingSenderId: "578563502406",
+    appId: "1:578563502406:web:bb538070abb7e8faa1f547"
+  };
+
+
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const db = getDatabase();
+
+  let form_log_in = document.getElementById('form_login');
+  form_log_in.addEventListener('submit', (e) => {
+    e.preventDefault();
+    var email = document.getElementById('user_email').value;
+    var password =  document.getElementById('user_password').value;
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    // Signed in 
+      const user = userCredential.user;	
+    // ...
+
+    var loginData = new Date()
+       update(ref(db, 'users/' + user.uid), {
+      lastLogin: loginData
+       })
+      .then(() => {
+      // Data saved successfully!
+    alert('User logged successfully')
+    window.location.replace("travel_blog.html");
+     })
+      .catch((error) => {
+     // The write failed...
+      alert('Error')
+    });
+})
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  alert(errorMessage);
+});
+
+
+});
+
+
+
+let btn_clear = document.getElementById('btn_clear');
+btn_clear.addEventListener('click', function(){
+let email = document.getElementById('user_email');
+var password =  document.getElementById('user_password');
+email.value = '';
+password.value = ''
+});
