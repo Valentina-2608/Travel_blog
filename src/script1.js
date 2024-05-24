13a334f8e41af08f154966bad696c010e4d87ca0
Valetina-2608 <valusha2608@gmail.com>
2024-05-24 19:47:27 +0300

    Change file

--- src/script1.js
+++ src/script1.js
@@ -1,11 +1,11 @@
 /* Registration page */
 
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
 
-  import { getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"
+  import { getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js"
 	
-  import { getDatabase, ref, set} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+  import { getDatabase, ref, set} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
